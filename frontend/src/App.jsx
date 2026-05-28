9d35838c8f68a5b819ce3d611ee082131f14332f
Prachijinal <prachi12jindal246@gmail.com>
2026-05-28 11:58:48 +0530

    Added search input UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div
       style={{
         textAlign: "center",
-        marginTop: "100px",
+        marginTop: "80px",
         fontFamily: "Arial",
       }}
     >
@@ -12,11 +12,37 @@ function App() {
       </h1>
 
       <p>
-        Find Bus Routes and Timings Easily
+        Search Bus Routes and Timings
       </p>
+
+      <div style={{ marginTop: "30px" }}>
+        <input
+          type="text"
+          placeholder="From"
+          style={{
+            padding: "10px",
+            width: "200px",
+            marginRight: "10px",
+            borderRadius: "5px",
+            border: "1px solid gray",
+          }}
+        />
+
+        <input
+          type="text"
+          placeholder="To"
+          style={{
+            padding: "10px",
+            width: "200px",
+            borderRadius: "5px",
+            border: "1px solid gray",
+          }}
+        />
+      </div>
 
       <button
         style={{
+          marginTop: "20px",
           padding: "10px 20px",
           backgroundColor: "green",
           color: "white",

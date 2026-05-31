d0f24acc9630130ccb6ac991dedc14e58e1bd292
Prachijinal <prachi12jindal246@gmail.com>
2026-05-31 12:30:50 +0530

    Connected frontend with backend API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const buses = require("./data/buses");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Haryana Roadways Backend Running");

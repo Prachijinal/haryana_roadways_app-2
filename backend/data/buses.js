b95335fcfcc4c36db4cd69dce6c7009ed5c65381
Prachijinal <prachi12jindal246@gmail.com>
2026-05-31 09:58:57 +0530

    Created first buses API

---
+++ backend/data/buses.js
@@ -0,0 +1,28 @@
+const buses = [
+  {
+    from: "Rohtak",
+    to: "Chandigarh",
+    time: "6:30 AM",
+    type: "AC",
+  },
+  {
+    from: "Rohtak",
+    to: "Delhi",
+    time: "8:00 AM",
+    type: "Ordinary",
+  },
+  {
+    from: "Panipat",
+    to: "Chandigarh",
+    time: "9:15 AM",
+    type: "Volvo",
+  },
+  {
+    from: "Hisar",
+    to: "Delhi",
+    time: "10:00 AM",
+    type: "AC",
+  },
+];
+
+module.exports = buses;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const buses = require("./data/buses");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Haryana Roadways Backend Running");
+});
+
+app.get("/api/buses", (req, res) => {
+  res.json(buses);
 });
 
 app.listen(5000, () => {
